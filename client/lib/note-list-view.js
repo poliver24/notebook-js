94f1b297e43ff0b5249c7e47c63e10f597f4bb15
Patrick Oliver <poliver24@hotmail.com>
2020-04-01 14:28:19 +0100

    l

--- client/lib/note-list-view.js
+++ client/lib/note-list-view.js
@@ -7,7 +7,7 @@ class NoteListView {
     if (this.noteList.notes.length === 0) {
       throw new Error("You have no notes to print");
     } else {
-      var list = "<ul><li><div><a href=" + this.noteList.notes.map(note => "#" + note.id + ">" + note.text.slice(0, 20)).join("</a></div></li><div><li><a href=") + "</a></div></li></ul>";
+      var list = "<ul><li><div><a href=#" + this.noteList.notes.map(note => note.id + ">" + note.text.slice(0, 20)).join("</a></div></li><div><li><a href=") + "</a></div></li></ul>";
       // document.getElementById("list").innerHTML = list;
       return list;
     };
